1499192443623c94761de2874afb0f41fcb01a3e
burr-add <96995785+burr-add@users.noreply.github.com>
2022-01-17 12:44:50 -0600

    PMI
    grabs PMI from ISM website

---
+++ PMILookUp.py
@@ -0,0 +1,10 @@
+import requests as rs
+
+from bs4 import BeautifulSoup
+
+page = rs.get('https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi'
+              '/december')
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup.find_all('h1')[0].get_text())
