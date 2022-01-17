b19075ca266f2b8abe21ec752024203242772059
burr-add <96995785+burr-add@users.noreply.github.com>
2022-01-17 13:32:22 -0600

    created 'headline'

--- PMILookUp.py
+++ PMILookUp.py
@@ -2,9 +2,10 @@ import requests as rs
 
 from bs4 import BeautifulSoup
 
-page = rs.get('https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi'
-              '/december')
+PMI_month = input("Enter month:")
+PMI_url = "https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi/"+str(PMI_month)
+page = rs.get(PMI_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 headline = soup.find_all('h1')[0].get_text()
 
-print(headline)
+print(str(PMI_month)+""+headline)
