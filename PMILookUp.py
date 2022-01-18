20f0598da1af13711ae7a9f1c551676caabc8beb
burr-add <96995785+burr-add@users.noreply.github.com>
2022-01-18 13:10:26 -0600

    created valid date range variables

--- PMILookUp.py
+++ PMILookUp.py
@@ -1,6 +1,14 @@
 import requests as rs
-
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from bs4 import BeautifulSoup
+
+# Establishes valid date range
+today = datetime.today()
+one_month_ago = (today - relativedelta(months = 1)).strftime("%B")
+two_month_ago = (today - relativedelta(months = 2)).strftime("%B")
+three_month_ago = (today - relativedelta(months = 3)).strftime("%B")
+valid_months = [one_month_ago, two_month_ago, three_month_ago]
 
 PMI_month = input("Enter month:")
 PMI_url = "https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi/"+str(PMI_month)
