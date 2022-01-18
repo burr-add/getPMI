11344c832271a724c36b189a043fae5d9399c070
burr-add <96995785+burr-add@users.noreply.github.com>
2022-01-18 17:49:30 -0600

    removed spaces

--- PMILookUp.py
+++ PMILookUp.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 
 # Establishes valid date range
 today = datetime.today()
-one_month_ago = (today - relativedelta(months = 1)).strftime("%B")
-two_month_ago = (today - relativedelta(months = 2)).strftime("%B")
-three_month_ago = (today - relativedelta(months = 3)).strftime("%B")
+one_month_ago = (today - relativedelta(months=1)).strftime("%B")
+two_month_ago = (today - relativedelta(months=2)).strftime("%B")
+three_month_ago = (today - relativedelta(months=3)).strftime("%B")
 valid_months = [one_month_ago, two_month_ago, three_month_ago]
 
 while True:
