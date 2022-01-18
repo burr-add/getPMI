4daa39aebdaeca97a02bb4f8a20ad4c55d2a96a7
burr-add <96995785+burr-add@users.noreply.github.com>
2022-01-18 17:35:07 -0600

    added while loop

--- PMILookUp.py
+++ PMILookUp.py
@@ -10,8 +10,15 @@ two_month_ago = (today - relativedelta(months = 2)).strftime("%B")
 three_month_ago = (today - relativedelta(months = 3)).strftime("%B")
 valid_months = [one_month_ago, two_month_ago, three_month_ago]
 
-PMI_month = input("Enter month:")
-PMI_url = "https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi/"+str(PMI_month)
+while True:
+    print("Enter month of desired PMI: ")
+    PMI_month = input()
+    if PMI_month not in valid_months:
+        print("Please enter a valid month")
+    else:
+        break
+
+PMI_url = 'https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi/' + str(PMI_month)
 page = rs.get(PMI_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 headline = soup.find_all('h1')[0].get_text()
